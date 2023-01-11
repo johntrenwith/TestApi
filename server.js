aed770e9e1b9e5343bb769a75eba7ef481074407
John Trenwith <john.trenwith@stjohn.org.nz>
2023-01-11 16:17:28 +1300

    fix: better option - add cors package

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
 // Require express
 const express = require("express");
+var cors = require('cors')
 // Initialize express
 const app = express();
 const PORT = 8080;
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
 
-module.exports = allowCors(handler)
+
 // parse JSON
 app.use(express.json());
+app.use(cors())
 // parse URL encoded data
 app.use(express.urlencoded({ extended: true }));
 
